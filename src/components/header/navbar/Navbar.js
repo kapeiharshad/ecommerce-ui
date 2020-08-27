28310bfc452392f039c6bfcde2f2c1c18507030e
kapeiharshad <kapeiharshad@gmail.com>
2020-08-28 01:17:39 +0530

    logo img and favicon added

--- src/components/header/navbar/Navbar.js
+++ src/components/header/navbar/Navbar.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Navbar.css";
+import logo from "../../../img/logoImg.png";
 
 const Navbar = () => {
-  return <div className="baggateLogo">Baggagte</div>;
+  return (
+    <div className="body">
+      <img src={logo} alt="BagGate Logo" />
+    </div>
+  );
 };
 
 export default Navbar;
