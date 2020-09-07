88933a5052d592d39d179b6b9592d853aa35331b
kapeiharshad <kapeiharshad@gmail.com>
2020-09-07 17:18:40 +0530

    navbar responsive done

--- src/components/header/navbar/Navbar.js
+++ src/components/header/navbar/Navbar.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./Navbar.css";
 import logo from "../../../img/logoImg.png";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="main-body" style={{paddingTop: "2rem"}}>
+    <div className="main-body" style={{ paddingTop: "2rem" }}>
       <div className="navbar section-center-navbar">
-        <div className="brandLogo">
+        <div className="brandLogo-container">
           <Link to="/">
-            <img src={logo} alt="BagGate Logo" />
+            <img src={logo} alt="BagGate Logo" className="brandLogo" />
           </Link>
         </div>
         <div className="nav-links-container">
@@ -24,27 +24,31 @@ const Navbar = () => {
             </button>
           </form>
 
-          {/* <div className="signInedTab">
+          <div className="signInedTab">
             <div>
               <span className="nav-text">
-                <i className="fas fa-shopping-bag nav-links-icon"></i>My orders
+                <i className="fas fa-shopping-bag nav-links-icon"></i>
+                <span className="links-text">My orders</span>
               </span>
               <span className="nav-text">
-                <i className="fas fa-power-off nav-links-icon"></i>Logout
-              </span>
-            </div>
-          </div> */}
-
-          <div className="signOutedTab">
-            <div>
-              <span className="nav-text">
-                <i className="fas fa-sign-in-alt nav-links-icon"></i>Sign In
-              </span>
-              <span className="nav-text">
-                <i className="fas fa-user-plus nav-links-icon"></i>Sign Up
+                <i className="fas fa-power-off nav-links-icon"></i>{" "}
+                <span className="links-text">Logout</span>
               </span>
             </div>
           </div>
+
+          {/* <div className="signOutedTab">
+            <div>
+              <span className="nav-text">
+                <i className="fas fa-sign-in-alt nav-links-icon"></i>
+                <span className="links-text">Sign In</span>
+              </span>
+              <span className="nav-text">
+                <i className="fas fa-user-plus nav-links-icon"></i>
+                <span className="links-text">Sign Up</span>
+              </span>
+            </div>
+          </div> */}
         </div>
       </div>
       <div className="underline"></div>
