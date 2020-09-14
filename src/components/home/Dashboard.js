643196e6ea1fc81e09d9404a4deee10efccca1f0
kapeiharshad <kapeiharshad@gmail.com>
2020-09-14 16:09:26 +0530

    new Dashboard in progress

--- src/components/home/Dashboard.js
+++ src/components/home/Dashboard.js
@@ -1,6 +1,7 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Header from "../header/Header";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import bagImage from "../../img/bannerImg.png";
 import "./Dashboard.css";
 
 class Dashboard extends Component {
@@ -15,14 +16,17 @@ class Dashboard extends Component {
                 Enter <span className="hero-subtitle">into</span> bags heaven
                 <span className="hero-subtitle">...</span>
               </h1>
+              <div className="hero-text-container">
+                <p className="hero-text">
+                  The most trendy and fashionable bags waiting for you
+                </p>
+                <Link to="/collections" className="hero-button">
+                  Shop Now
+                </Link>
+              </div>
             </div>
-            <div className="hero-text-container">
-              <p className="hero-text">
-                The most trendy and fashionable bags waiting for you
-              </p>
-              <Link to="/collections" className="hero-button">
-                Shop Now
-              </Link>
+            <div className="bagImage-container">
+              <img src={bagImage} alt="Bags" className="bagImage" />
             </div>
           </div>
         </div>
