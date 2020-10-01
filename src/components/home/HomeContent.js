7f986978b73ab8e318ef1509b5bcbe6446b7c382
kapeiharshad <kapeiharshad@gmail.com>
2020-10-01 22:10:28 +0530

    feature created

--- src/components/home/HomeContent.js
+++ src/components/home/HomeContent.js
@@ -6,29 +6,26 @@ class HomeContent extends Component {
   render() {
     return (
       <div className="home-content">
-        <div className="features-container">
+        <div className="features-container section-center-feature">
           <div className="single-feature">
             <h4 className="features-title">100% Genuine Products</h4>
             <p className="features-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut earum
-              exercitationem doloremque minus ipsa quam fugit nemo ratione
-              voluptates placeat?
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed ex
+              ullam ipsa natus qui.
             </p>
           </div>
           <div className="single-feature">
-            <h4 className="features-title">100% Genuine Products</h4>
+            <h4 className="features-title">Safe & Secure Payments</h4>
             <p className="features-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut earum
-              exercitationem doloremque minus ipsa quam fugit nemo ratione
-              voluptates placeat?
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed ex
+              ullam ipsa natus qui.
             </p>
           </div>
           <div className="single-feature">
-            <h4 className="features-title">100% Genuine Products</h4>
+            <h4 className="features-title">Money Back Guarantee</h4>
             <p className="features-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut earum
-              exercitationem doloremque minus ipsa quam fugit nemo ratione
-              voluptates placeat?
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed ex
+              ullam ipsa natus qui.
             </p>
           </div>
         </div>
