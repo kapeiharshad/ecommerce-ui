f04288e9a163a950297ef21d5085d17c3408bd4a
kapeiharshad <kapeiharshad@gmail.com>
2020-10-02 15:17:57 +0530

    component to function

--- src/components/home/Feature.js
+++ src/components/home/Feature.js
@@ -1,37 +1,34 @@
-import React, {Component} from "react";
-// import {Link} from "react-router-dom";
+import React from "react";
 import "./Feature.css";
 
-class Feature extends Component {
-  render() {
-    return (
-      <div className="home-content">
-        <div className="features-container section-center-feature">
-          <div className="single-feature">
-            <h4 className="features-title">100% Genuine Products</h4>
-            <p className="features-text">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed ex
-              ullam ipsa natus qui.
-            </p>
-          </div>
-          <div className="single-feature">
-            <h4 className="features-title">Safe & Secure Payments</h4>
-            <p className="features-text">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed ex
-              ullam ipsa natus qui.
-            </p>
-          </div>
-          <div className="single-feature">
-            <h4 className="features-title">Money Back Guarantee</h4>
-            <p className="features-text">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed ex
-              ullam ipsa natus qui.
-            </p>
-          </div>
+const Feature = () => {
+  return (
+    <div className="home-content">
+      <div className="features-container section-center-feature">
+        <div className="single-feature">
+          <h4 className="features-title">100% Genuine Products</h4>
+          <p className="features-text">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed ex
+            ullam ipsa natus qui.
+          </p>
+        </div>
+        <div className="single-feature">
+          <h4 className="features-title">Safe & Secure Payments</h4>
+          <p className="features-text">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed ex
+            ullam ipsa natus qui.
+          </p>
+        </div>
+        <div className="single-feature">
+          <h4 className="features-title">Money Back Guarantee</h4>
+          <p className="features-text">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed ex
+            ullam ipsa natus qui.
+          </p>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Feature;
