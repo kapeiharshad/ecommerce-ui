ee9158eecde29b8c7e9b0463191931e18f0bb825
kapeiharshad <kapeiharshad@gmail.com>
2020-10-02 21:30:38 +0530

    main gallery responsive done

--- src/components/home/Gallery.js
+++ src/components/home/Gallery.js
@@ -1,5 +1,7 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./Gallery.css";
+import { Link } from "react-router-dom";
+
 import mainImg from "../../img/main-galleryImg.jpg";
 
 class Gallery extends Component {
@@ -9,6 +11,17 @@ class Gallery extends Component {
         <div className="gallery-container">
           <div className="main-gallery-container">
             <img src={mainImg} alt="main gallery Img" className="main-image" />
+          </div>
+          <div className="main-gallery-text-container">
+            <i className="fas fa-crown main-gallery-icon"></i>
+            <h4 className="main-gallery-title">Luxury at its finest</h4>
+            <p className="main-gallery-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              sunt.
+            </p>
+            <Link to="/collections" className="main-btn">
+              Shop Now
+            </Link>
           </div>
         </div>
       </div>
