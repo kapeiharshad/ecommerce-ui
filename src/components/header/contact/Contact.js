33a7f5b7451d63f50982862ddfc3a0c6064df7d5
kirtikapei <krtkapei@gmail.com>
2020-10-06 18:07:23 +0500

    contact created

--- src/components/header/contact/Contact.js
+++ src/components/header/contact/Contact.js
@@ -5,6 +5,47 @@ const Contact = () => {
       <div className="contacttitle">
       Contact <span className="sub-title">us</span>  
      </div>
+     <div className="contact_form">
+      <form action="">
+        <div>
+          <input
+            type="username"
+            placeholder="Username"
+            className="input-box"
+          />
+        </div>
+        <div>
+          <input
+            type="useremail"
+            placeholder="Useremail"
+            className="input-box"
+          />
+        </div>
+        <div>
+          <input
+            type="userphone"
+            placeholder="Userphone"
+            className="input-box"
+          />
+        </div>
+        
+        <div>
+          <textarea
+            id="message"
+            placeholder="Enter your message here"
+            className="textarea-box"
+          />
+        </div>
+        <button className="secoundary-btn">Submit</button>
+
+        
+
+
+
+
+
+      </form>
+     </div>
 
      
 
