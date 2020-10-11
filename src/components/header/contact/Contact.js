d8d770e823c58791aa6fba23865f26f7169814fd
kapeiharshad <kapeiharshad@gmail.com>
2020-10-11 17:33:29 +0530

    contact in progress

--- src/components/header/contact/Contact.js
+++ src/components/header/contact/Contact.js
@@ -1,55 +1,47 @@
 import React from "react";
 import "./Contact.css";
 const Contact = () => {
-    return <div className="contact main-body">
-      <div className="contacttitle">
-      Contact <span className="sub-title">us</span>  
-     </div>
-     <div className="contact_form">
-      <form action="">
-        <div>
-          <input
-            type="username"
-            placeholder="Username"
-            className="input-box"
-          />
-        </div>
-        <div>
-          <input
-            type="useremail"
-            placeholder="Useremail"
-            className="input-box"
-          />
-        </div>
-        <div>
-          <input
-            type="userphone"
-            placeholder="Userphone"
-            className="input-box"
-          />
-        </div>
-        
-        <div>
-          <textarea
-            id="message"
-            placeholder="Enter your message here"
-            className="textarea-box"
-          />
-        </div>
-        <button className="secoundary-btn">Submit</button>
+  return (
+    <div className="contact main-body">
+      <h3 className="main-title">
+        Contact <span className="sub-title">us</span>
+      </h3>
+      <div className="contact_form">
+        <form action="">
+          <div>
+            <input
+              type="username"
+              placeholder="Username"
+              className="input-box"
+            />
+          </div>
+          <div>
+            <input
+              type="useremail"
+              placeholder="Useremail"
+              className="input-box"
+            />
+          </div>
+          <div>
+            <input
+              type="userphone"
+              placeholder="Userphone"
+              className="input-box"
+            />
+          </div>
 
-        
+          <div>
+            <textarea
+              id="message"
+              placeholder="Enter your message here"
+              className="textarea-box"
+            />
+          </div>
+          <button className="secoundary-btn">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-
-
-
-
-      </form>
-     </div>
-
-     
-
-      </div>;
-  };
-  
-  export default Contact;
+export default Contact;
